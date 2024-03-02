abe1531a18eb02338d0621f49923ab4903392fbe
daudln <daudnamayala@gmail.com>
2024-03-02 10:34:21 +0300

    CORS

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import joblib
 import pandas as pd
 from schema.input import InputData
 
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost",
+    "http://localhost:8081",
+]
+
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/predict/")
